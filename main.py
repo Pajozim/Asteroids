6ea9ad202fc55e53a9a5d73ee3178fbbbb6bf81a
git_PaJo <pajozim@gmail.com>
2025-05-22 20:41:35 -0600

    B 0525: asteroids and spaceship

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
 import pygame
 #from database import connect_database, database_version
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
   pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  clock = pygame.time.Clock()
+  dt = 0
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT / 2
+
+  #groups
+  g_updatable = pygame.sprite.Group()
+  g_drawable = pygame.sprite.Group()
+
+  Player.containers = (g_updatable, g_drawable)
+
+  player = Player(x, y, PLAYER_RADIUS)
+
+  #--
+  AsteroidField.containers = (g_updatable,)
+
+  asteroid_field = AsteroidField()
+
+  g_asteroids = pygame.sprite.Group()
+
+  Asteroid.containers = (g_asteroids, g_updatable, g_drawable)
+
+  #end of grouping
 
   while True:
     pygame.Surface.fill(screen, (0,0,0))
+    for py_object in g_updatable:
+      py_object.update(dt)
+    for py_object in g_drawable:
+      py_object.draw(screen)  # Drawing each object in the group
     pygame.display.flip()
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
-        return
-
+          return
+  
+    clock.tick(60)
+    dt = clock.tick(60) / 1000
+  
+  #end of game loop
 
 
 if __name__ == "__main__":
